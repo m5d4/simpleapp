520bb1d6124b0a15c01b7148cd34ba3aa4202478
m5d4 <d.mail@centrum.cz>
2022-09-16 13:44:02 +0200

    hello world

--- index.php
+++ index.php
@@ -5,12 +5,5 @@
 	</head>
 
 <body> 
-	<h1>PHP Test</h1>
-		<p><b>An Example of PHP in Action</b></p>
-		<?php echo "The Current Date and Time is: <br />"; 
-		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information</h2> 
-		<p> <?php phpinfo(); ?> </p> 
-        <h2>GOING ON VACATION</h2>
-	</body> 
+	<h1>Hello World</h1>
 </html>
